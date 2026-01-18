6038149935c147e79be5eff150c6981bab5965ae
cadamsdev <cadamsdev@outlook.com>
2026-01-18 10:37:15 -0500

    use interactive fullscreen mode

--- index.ts
+++ index.ts
@@ -4,5 +4,26 @@ import React from "react";
 import { render } from "ink";
 import { App } from "./src/App";
 
+// Enable alternate screen buffer (fullscreen mode)
+process.stdout.write("\x1b[?1049h");
+
 // Render the app
-render(React.createElement(App));
+const app = render(React.createElement(App));
+
+// Cleanup: Restore normal screen buffer on exit
+const cleanup = () => {
+  process.stdout.write("\x1b[?1049l");
+};
+
+app.waitUntilExit().then(cleanup);
+
+// Handle unexpected exits
+process.on("exit", cleanup);
+process.on("SIGINT", () => {
+  cleanup();
+  process.exit(0);
+});
+process.on("SIGTERM", () => {
+  cleanup();
+  process.exit(0);
+});
